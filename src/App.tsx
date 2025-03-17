3de51b742777bd40e46be1e8a88f7ddc1e683358
bugra-oner <bugraaoner@gmail.com>
2025-03-18 01:34:06 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import reactLogo from "../Header/test.svg";
+
+export default function Header() {
+  return (
+    <div>
+      Header
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="services">Services</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
