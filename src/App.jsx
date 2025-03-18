5408bae03ac5ed8e71a5d8bc8609a224dd7c35ba
bugra-oner <bugraaoner@gmail.com>
2025-03-18 20:46:12 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        {/*<Route path="/Home" element={<Home />} />*/}
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Home.css";
 
 export default function Home() {
-
   console.log("Test");
 
   return (
@@ -10,14 +9,14 @@ export default function Home() {
       {/* Hero Section (Tanıtım Videosu) */}
       <section className="hero">
         <video className="hero-video" autoPlay muted loop>
-          <source src="/videos/madcar-intro.mp4" type="video/mp4" />
+          <source src="/videos/sl_1.mp4" type="video/mp4" />
           Tarayıcınız video oynatmayı desteklemiyor.
         </video>
         <div className="hero-text">
           <h1>Madcar Yıkama Hizmetleri</h1>
           <p>
-            Aracınız için en kaliteli ve hızlı temizlik hizmeti burada. 
-            Yüksek kaliteli temizlik ve profesyonel ekip!
+            Aracınız için en kaliteli ve hızlı temizlik hizmeti burada. Yüksek
+            kaliteli temizlik ve profesyonel ekip!
           </p>
           <button>Hizmetlerimize Göz At</button>
         </div>
